988f087a4e6d2a28abd9f7c7f2db5f41a465b30e
vihwan <vihwan@ajou.ac.kr>
2019-07-09 15:46:26 +0900

    Add HttpConfig.java

--- weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/controller/WeatherController.java
+++ weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/controller/WeatherController.java
@@ -3,6 +3,7 @@ package org.ajou.realcoding.weathercrawler.weathercrawler.controller;
 import org.ajou.realcoding.weathercrawler.weathercrawler.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -14,10 +15,15 @@ public class WeatherController {
     @Autowired
     WeatherService weatherService;
 
-    @GetMapping("/whether-crawler/available-cities")
+    @GetMapping("/weather-crawler/available-cities")
     public List<String> getAvailableCities() throws IOException {
 
         return weatherService.loadAvailableCityNamesFromFile();
+    }
 
+    @GetMapping("/weather-crawler/current-weather/by-city-name/{cityName}")
+    public String getCurrentWeather(@PathVariable String cityName){
+        return weatherService.getCurrentWeatherByCities(cityName);
     }
 }
+

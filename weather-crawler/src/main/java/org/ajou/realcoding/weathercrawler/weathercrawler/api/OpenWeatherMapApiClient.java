266acf88f273093e34a97f6d9b59399f3bd80f74
vihwan <vihwan@ajou.ac.kr>
2019-07-09 16:16:43 +0900

    Change requestCurrentWeather Type to "CurrentWeather"

--- weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/api/OpenWeatherMapApiClient.java
+++ weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/api/OpenWeatherMapApiClient.java
@@ -1,6 +1,7 @@
 package org.ajou.realcoding.weathercrawler.weathercrawler.api;
 
 
+import org.ajou.realcoding.weathercrawler.weathercrawler.domain.CurrentWeather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,9 @@ public class OpenWeatherMapApiClient {
     @Autowired
     RestTemplate restTemplate;
 
-    public String requestCurrentWeather(String cityName) {
+    public CurrentWeather requestCurrentWeather(String cityName) {
         //exchange method
-        return restTemplate.exchange(openWeatherUrl, HttpMethod.GET,null, String.class, cityName, appid)
+        return restTemplate.exchange(openWeatherUrl, HttpMethod.GET,null, CurrentWeather.class, cityName, appid)
                 .getBody(); //응답받은 response의 body를 꺼내서 리턴
     }
 }

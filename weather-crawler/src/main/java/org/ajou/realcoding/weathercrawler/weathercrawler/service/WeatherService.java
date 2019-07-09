045cf8b636897e27ecd8d8328ddfe337d7efeeeb
vihwan <vihwan@ajou.ac.kr>
2019-07-09 15:45:44 +0900

    Add WeatherService.java

--- weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/service/WeatherService.java
+++ weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/service/WeatherService.java
@@ -3,23 +3,37 @@ package org.ajou.realcoding.weathercrawler.weathercrawler.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ajou.realcoding.weathercrawler.weathercrawler.api.OpenWeatherMapApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @Service
 public class WeatherService {
+
+    List<String> cities = null;
+
     /**Spring boot의 라이브러리인 ObjectMapper를 사용
     */
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    OpenWeatherMapApiClient openWeatherMapApiClient;
 
+    @PostConstruct
     public List<String> loadAvailableCityNamesFromFile() throws IOException {
-        File file = new File("availalbeCityNames");
+        File file = new File("availableCityNames");
 
         return objectMapper.readValue(file, new TypeReference<List<String>>(){});
+    }
+
+    public String getCurrentWeatherByCities(String cityName){
+        return openWeatherMapApiClient.requestCurrentWeather(cityName);
+
     }
 }

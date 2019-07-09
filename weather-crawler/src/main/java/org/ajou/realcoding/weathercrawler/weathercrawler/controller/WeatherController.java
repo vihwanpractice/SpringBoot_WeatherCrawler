8f0a34e2b339a4777073599a5b844777bb4a52f9
vihwan <vihwan@ajou.ac.kr>
2019-07-09 16:17:11 +0900

    Change getCurrentWeather Type to "CurrentWeather"

--- weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/controller/WeatherController.java
+++ weather-crawler/src/main/java/org/ajou/realcoding/weathercrawler/weathercrawler/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package org.ajou.realcoding.weathercrawler.weathercrawler.controller;
 
+import org.ajou.realcoding.weathercrawler.weathercrawler.domain.CurrentWeather;
 import org.ajou.realcoding.weathercrawler.weathercrawler.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +23,7 @@ public class WeatherController {
     }
 
     @GetMapping("/weather-crawler/current-weather/by-city-name/{cityName}")
-    public String getCurrentWeather(@PathVariable String cityName){
+    public CurrentWeather getCurrentWeather(@PathVariable String cityName){
         return weatherService.getCurrentWeatherByCities(cityName);
     }
 }
